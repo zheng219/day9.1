11aea41e1e8e07b0e9d392e9be988adec2ac34f8
zth <zth>
2017-09-01 16:30:01 +0800

    更改

--- js/script.js
+++
@@ -1,5 +0,0 @@
-;$(".center").on("click","span",function(){
-	var idx=$(this).index();
-	$(this).addClass('on').siblings().removeClass('on');
-	$(".main").find("ul").eq(idx).show().siblings().hide();
-})
